52cabf319b270a11ebd51a8e0c1ac1dbaecb21e9
dsxailab <dsxailab@users.noreply.github.com>
2025-05-23 05:59:51 +0000

    deploy: d4f7e54098d0b8a8a88fa4d84b8b6be6bc9bcf32

---
+++ my-script.meta.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         DeleteAllPixVerse
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-20
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.pixverse.ai/asset/video
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pixverse.ai
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function start(){
+        const selectBtn = document.getElementsByClassName("text-text-secondary text-sm flex")[0];
+        selectBtn.click();
+
+        setTimeout(() => {
+            const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach(checkbox => {
+                //realClick(checkbox);
+                checkbox.click();
+
+                // Optional: dispatch a change event in case something listens for it
+                checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+            });
+            const delButton = document.getElementsByClassName("text-text-secondary text-sm flex")[0];
+            delButton.click();
+
+            setTimeout(() => {
+                console.log("confirming");
+                const confirm = document.getElementsByClassName('ant-btn ant-btn-dangerous')[0];
+                confirm.click();
+
+                setTimeout(() => start(), 3000);
+            }, 1000);
+        },3000);
+    }
+     // Create a container div for the toolbar
+    const bar = document.createElement('div');
+    bar.style.marginTop = '140px';
+    bar.style.marginLeft = '150px'
+    bar.style.position = 'relative';
+    bar.style.width = '50%';
+    bar.style.background = '#28a74500';
+    bar.style.color = 'white';
+    bar.style.padding = '10px';
+    bar.style.textAlign = 'center';
+    bar.style.fontFamily = 'sans-serif';
+    bar.style.boxShadow = '0 2px 5px rgba(0,0,0,0.3)';
+    bar.style.zIndex = '10000';
+
+
+    const button = document.createElement('button');
+    button.textContent = '✔️ Delete All';
+    button.style.position = 'fixed';
+    button.style.top = '250px';
+    button.style.right = '20px';
+    button.style.zIndex = '9999';
+    button.style.padding = '10px';
+    button.style.backgroundColor = '#28a745';
+    button.style.color = 'white';
+    button.style.border = 'none';
+    button.style.borderRadius = '5px';
+    button.style.cursor = 'pointer';
+    button.style.boxShadow = '0 2px 5px rgba(0,0,0,0.3)';
+
+    // Add click event to check all checkboxes
+    button.addEventListener('click', () => {
+        start();
+    });
+
+    // Add the button to the bar and the bar to the top of the body
+    bar.appendChild(button);
+    document.body.insertBefore(bar, document.body.firstChild);
+
+})();
