bc04361b16e62bf08ad4b7652780baef400498b9
dsxailab <dsxailab@users.noreply.github.com>
2025-05-23 06:01:47 +0000

    deploy: 84080497dd6b6c7496e05490704f4c87e8d6ccd1

--- DeleteAllPixVerse.js
+++ DeleteAllPixVerse.js
@@ -7,6 +7,8 @@
 // @match        https://app.pixverse.ai/asset/video
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pixverse.ai
 // @grant        none
+// @updateURL    https://dsxailab.github.io/PixVerseScript/DeleteAllPixVerse.js
+// @downloadURL  https://dsxailab.github.io/PixVerseScript/DeleteAllPixVerse.js
 // ==/UserScript==
 
 (function() {
