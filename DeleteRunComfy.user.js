486d3154253dc5c023cfa0f05d000ea5ed04f7e6
dsxailab <dsxailab@users.noreply.github.com>
2025-12-09 03:01:23 +0000

    deploy: ac1c91606110b144902fdf1b492b30c41d63c45e

---
+++ DeleteRunComfy.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name         DeleteRunComfy
+// @namespace    http://tampermonkey.net/
+// @version      2025.12.9.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.runcomfy.com/playground/assets
+// @icon         https://www.runcomfy.com/_next/image?url=%2Fassets%2Fimages%2Flogo.png&w=256&q=75
+// @grant        none
+// @updateURL    https://dsxailab.github.io/PixVerseScript/DeleteRunComfy.user.js
+// @downloadURL  https://dsxailab.github.io/PixVerseScript/DeleteRunComfy.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function start() {
+        const toolbarQuery = "button div.shrink-0";
+        /*
+        document.getElementsByTagName("video")[0].click();
+        await sleep(1000);
+
+        const delButton = Array.from(document.querySelectorAll(toolbarQuery)).find(el => el.textContent.trim() === 'Delete');
+        delButton.click();
+
+        await sleep(1000);
+
+        console.log("confirming");
+        const confirm = document.querySelectorAll("div[role='dialog'][data-state='open'] button")[1];
+        confirm.click();
+
+        await sleep(2000);
+
+        start();
+        */
+        const selectBtn = Array.from(document.querySelectorAll(toolbarQuery)).find(el => el.textContent.trim() === 'Select');
+        selectBtn.click();
+
+        await sleep(3000);
+
+        const checkboxes = document.querySelectorAll('button[role="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.click();
+
+            // Optional: dispatch a change event in case something listens for it
+            checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+        });
+        await sleep(100); // give ui chance to update the checkbox states
+        const delButton = Array.from(document.querySelectorAll(toolbarQuery)).find(el => el.textContent.trim() === 'Delete');
+        delButton.click();
+        await sleep(1000);
+
+        console.log("confirming");
+        const confirm = document.querySelectorAll("div[role='dialog'][data-state='open'] button")[1];
+        confirm.click();
+
+        setTimeout(() => start(), 3000);//*/
+    }
+     // Create a container div for the toolbar
+    const bar = document.createElement('div');
+    bar.style.marginTop = '140px';
+    bar.style.marginLeft = '150px'
+    bar.style.position = 'relative';
+    bar.style.width = '50%';
+    bar.style.background = '#28a74500';
+    bar.style.color = 'white';
+    bar.style.padding = '10px';
+    bar.style.textAlign = 'center';
+    bar.style.fontFamily = 'sans-serif';
+    bar.style.boxShadow = '0 2px 5px rgba(0,0,0,0.3)';
+    bar.style.zIndex = '10000';
+
+
+    const button = document.createElement('button');
+    button.textContent = '✔️ Delete All';
+    button.style.position = 'fixed';
+    button.style.top = '250px';
+    button.style.right = '20px';
+    button.style.zIndex = '9999';
+    button.style.padding = '10px';
+    button.style.backgroundColor = '#28a745';
+    button.style.color = 'white';
+    button.style.border = 'none';
+    button.style.borderRadius = '5px';
+    button.style.cursor = 'pointer';
+    button.style.boxShadow = '0 2px 5px rgba(0,0,0,0.3)';
+
+    // Add click event to check all checkboxes
+    button.addEventListener('click', () => {
+        start();
+    });
+
+    // Add the button to the bar and the bar to the top of the body
+    bar.appendChild(button);
+    document.body.insertBefore(bar, document.body.firstChild);
+
+})();
